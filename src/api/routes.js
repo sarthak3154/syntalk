8417e6a43b5ddaf5b18286d0eddc5454472c93aa
sarthak3154 <sarthak.arora3154@gmail.com>
2018-11-24 17:11:19 -0500

    socket.io audio streamed

--- src/api/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const translateController = require('./Controllers/TranslateController');
-
-router.post('/v1/textTranslate', translateController.translateText);
-
-module.exports = router;
